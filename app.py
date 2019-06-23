94a9130ccc5b62e7b84c18c8d4af438c2a2a18ff
guozi2 <lewoking@gmail.com>
2019-06-24 07:08:12 +0800

    nodb wait

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, render_template, session, redirect
 #import numpy as np
 import pandas as pd
-from replacexx import getdata
+#from replacexx import getdata
 
 
 app = Flask(__name__)
--- replacexx.py
+++ replacexx.py
@@ -6,15 +6,23 @@
 @Version :   1.0
 @WebSite    :  github.com/lewoking
 '''
-import pandas
+import pandas as pd
+
+from sqlalchemy import create_engine
+
 # here put the import lib
 def getdata(df)：
+    conn = create_engine('mysql+pymysql://root:usbw@172.0.0.1:3307/jmjkxx?charset=utf8')
     hf = operate
     str_bh = username
+    sql = 'SELECT obj_dispidx as ' #查询线路名称
+    sqlr = ''      #默认结果
     
-
+    df = pd.read_sql(sql, conn)
+    pd.io.sql.to_sql(df, 'tzqk', con=conn, if_exists='append', index=False) #写入历史数据库
 
     str_zmc = '220kV枣山变掇枣线'
+
     list_bzh = ('合上××变××线××断路器',
                 '检查××变××线××断路器监控指示为合位',
                 '检查××变××线××断路器电流指示正常',
@@ -31,3 +39,10 @@ def getdata(df)：
         print('error')
     list_cz = [c.replace('××', str_bh) for c in list_cz]
     print(list_cz)
+    #list转pd
+    data=DataFrame(list_cz)
+    data=data.T  #转置之后得到想要的结果
+    data.rename(columns={0:'a',1:'b'},inplace=True)  #注意这里0和1都不是字符串
+    
+    df = pd.read_sql(sqlr, conn1)
+    conn.dispose()
