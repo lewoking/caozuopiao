a32d127d927a69eb5f066a02ca9a07a49f4ffef5
guozi2 <lewoking@gmail.com>
2019-06-25 23:35:38 +0800

    debug

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
-from flask import Flask, request, render_template, session, redirect
-#import numpy as np
+from flask import Flask, request, render_template
 import pandas as pd
-#from replacexx import getdata
+from sqlalchemy import create_engine
 
 
 app = Flask(__name__)
@@ -16,13 +15,54 @@ def html_table():
 @app.route('/',methods=['POST'])
 def search():
     name = request.form['username'].strip()#strip去除前后空格
+    operate = request.form['operate']
     if name:
-        return send_post(name)
+        return send_post(operate,name)
     else:
         return render_template('search.html', tips="请输入正确的双编号")
-def send_post(name):
-    df = pd.DataFrame({'A': [0, 1, 2, 3, 4],
-                   'C': ['y', 'e', 's', 'good', 'luck要长长吃吃吃吃吃吃静静静静']})
+def send_post(operate,name):
+    df = getdata(operate,name)
     return render_template('search.html',  tables=[df.to_html(classes='data')], titles=df.columns.values)
+
+def getdata(operate, username): 
+    conn = create_engine(
+        'mysql+pymysql://root:usbw@127.0.0.1:3306/jmjkxx?charset=utf8')
+    hf = operate  # 操作接口
+    str_bh = 'username'  # 查询输入接口
+    sql = 'SELECT concat(`站名`,`名称2`) FROM `荆门开关表` WHERE `编号`=\'' + \
+        str_bh + '\' limit 1 '  # 查询线路名称
+    sqlr = 'SELECT * FROM `操作` limit 0,20'  # 默认结果
+    res = conn.execute(sql)
+    row = res.fetchone()
+
+    str_zmc = row[0]  # 获取查询结果
+
+    list_bzh = ('合上××变××线××断路器',
+                '检查××变××线××断路器监控指示为合位',
+                '检查××变××线××断路器电流指示正常',
+                '检查××变××线功率正常。')
+    list_bzf = ('断开××变××线××断路器',
+                '检查××变××线××断路器监控指示为分位',
+                '检查××变××线××断路器电流指示为零',
+                    '检查××变××线功率为零。')
+    if hf == '合上':
+        list_cz = [c.replace('××变××线', str_zmc) for c in list_bzh]
+    elif hf == '断开':
+        list_cz = [c.replace('××变××线', str_zmc) for c in list_bzf]
+    else:
+        print('error')
+    list_cz = [c.replace('××', str_bh) for c in list_cz]
+    print(list_cz)
+    # list转pd
+    data = pd.DataFrame(list_cz)
+    data.rename(columns={0: '操作项目'}, inplace=True)  # 注意这里0和1都不是字符串
+
+    pd.io.sql.to_sql(data, '操作', con=conn, if_exists='append',
+                        index=False)  # 写入历史数据库
+
+    df = pd.read_sql(sqlr, conn)
+    conn.dispose()
+    return df
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
--- replacexx.py
+++ replacexx.py
@@ -7,21 +7,24 @@
 @WebSite    :  github.com/lewoking
 '''
 import pandas as pd
-
+import sys
 from sqlalchemy import create_engine
 
 # here put the import lib
-def getdata(df)：
-    conn = create_engine('mysql+pymysql://root:usbw@172.0.0.1:3307/jmjkxx?charset=utf8')
-    hf = operate
-    str_bh = username
-    sql = 'SELECT obj_dispidx as ' #查询线路名称
-    sqlr = ''      #默认结果
-    
-    df = pd.read_sql(sql, conn)
-    pd.io.sql.to_sql(df, 'tzqk', con=conn, if_exists='append', index=False) #写入历史数据库
 
-    str_zmc = '220kV枣山变掇枣线'
+
+def getdata(operate, username): 
+    conn = create_engine(
+        'mysql+pymysql://root:usbw@127.0.0.1:3306/jmjkxx?charset=utf8')
+    hf = operate  # 操作接口
+    str_bh = 'username'  # 查询输入接口
+    sql = 'SELECT concat(`站名`,`名称2`) FROM `荆门开关表` WHERE `编号`=\'' + \
+        str_bh + '\' limit 1 '  # 查询线路名称
+    sqlr = 'SELECT * FROM `操作` limit 0,20'  # 默认结果
+    res = conn.execute(sql)
+    row = res.fetchone()
+
+    str_zmc = row[0]  # 获取查询结果
 
     list_bzh = ('合上××变××线××断路器',
                 '检查××变××线××断路器监控指示为合位',
@@ -30,7 +33,7 @@ def getdata(df)：
     list_bzf = ('断开××变××线××断路器',
                 '检查××变××线××断路器监控指示为分位',
                 '检查××变××线××断路器电流指示为零',
-                '检查××变××线功率为零。')
+                    '检查××变××线功率为零。')
     if hf == '合上':
         list_cz = [c.replace('××变××线', str_zmc) for c in list_bzh]
     elif hf == '断开':
@@ -39,10 +42,13 @@ def getdata(df)：
         print('error')
     list_cz = [c.replace('××', str_bh) for c in list_cz]
     print(list_cz)
-    #list转pd
-    data=DataFrame(list_cz)
-    data=data.T  #转置之后得到想要的结果
-    data.rename(columns={0:'a',1:'b'},inplace=True)  #注意这里0和1都不是字符串
-    
-    df = pd.read_sql(sqlr, conn1)
+    # list转pd
+    data = pd.DataFrame(list_cz)
+    data.rename(columns={0: '操作项目'}, inplace=True)  # 注意这里0和1都不是字符串
+
+    pd.io.sql.to_sql(data, '操作', con=conn, if_exists='append',
+                        index=False)  # 写入历史数据库
+
+    df = pd.read_sql(sqlr, conn)
     conn.dispose()
+    return df
